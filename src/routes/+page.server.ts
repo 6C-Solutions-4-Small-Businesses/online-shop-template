383c313ba2b2950d11324836c41beca46309a8a9
H.Cissé <13452797+hamadycisse@users.noreply.github.com>
2024-05-27 10:06:11 +0000

    ci(frontend): Enable Incremental Static Rerendering on Home and Contact pages

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,7 +1,13 @@
 import type {PageServerLoad} from './$types'
 import type {CollectionPresentation} from '$lib/frontend/presentations/CollectionPresentation'
+import {FORCE_INCREMENTAL_STATIC_REGENERATION_TOKEN} from '$env/static/private'
 
-export const prerender = true
+export const config = {
+    isr: {
+        expiration: false,
+        bypassToken: FORCE_INCREMENTAL_STATIC_REGENERATION_TOKEN,
+    },
+}
 
 export const load: PageServerLoad = async (event): Promise<{ collections: CollectionPresentation[] }> => {
     let response: Response
