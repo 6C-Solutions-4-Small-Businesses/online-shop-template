d5020709316eb812f15c5118cc690f260f8c8721
Adderly Polanco <adderly.polanco@lp360.com>
2024-05-30 23:55:52 -0400

    style(change): Create a color palette

--- config/ScreenSizeConfig.ts
+++ config/ScreenSizeConfig.ts
@@ -1,6 +1,6 @@
-export const SM_SCREEN_SIZE: number = 480;
-export const MD_SCREEN_SIZE: number = 768;
-export const LG_SCREEN_SIZE: number = 1024;
-export const XL_SCREEN_SIZE: number = 1200;
-export const DOUBLE_XL_SCREEN_SIZE: number = 1440;
-export const TRIPLE_XL_SCREEN_SIZE: number = 1920;
+export const SM_SCREEN_SIZE: number = 600;
+export const MD_SCREEN_SIZE: number = 960;
+export const LG_SCREEN_SIZE: number = 1120;
+export const XL_SCREEN_SIZE: number = 1440;
+export const DOUBLE_XL_SCREEN_SIZE: number = 1920;
+export const TRIPLE_XL_SCREEN_SIZE: number = 2560;
